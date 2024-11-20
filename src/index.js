b2843c27b79944ce4975a55cfa8ee21dda05f02c
doneni <xxerrenn@icloud.com>
2024-11-20 10:58:35 +0900

    fix: resolve React 18 root.render warning

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import './assets/styles/index.css';
 import './assets/styles/variables.css';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById(root)
+  </React.StrictMode>
 );
