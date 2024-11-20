b56ca433b1cdd4dee02804d3ef2c57c3519ee068
doneni <xxerrenn@icloud.com>
2024-11-20 10:42:09 +0900

    feat: auth contexts managing token

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
 
@@ -6,11 +6,33 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [token, setToken] = useState(null);
 
-    const toggleLogin = () => setIsLoggedIn(prev => !prev);
+    const login = (newToken) => {
+        localStorage.setItem('token', newToken);
+        setToken(newToken);
+        console.log(newToken)
+        setIsLoggedIn(true);
+        console.log('login')
+    };
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        setToken(null);
+        setIsLoggedIn(false);
+        console.log('logout')
+    };
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        if (storedToken) {
+            setToken(storedToken);
+            setIsLoggedIn(true);
+        }
+    }, []);
 
     return (
-        <AuthContext.Provider value={{ isLoggedIn, toggleLogin }}>
+        <AuthContext.Provider value={{ isLoggedIn, token, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
