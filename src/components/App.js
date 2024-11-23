46f93d03926d87c910fa087d5ed928cbfd74f5fd
doneni <xxerrenn@icloud.com>
2024-11-24 05:20:28 +0900

    chore: update .gitignore

--- src/components/App.js
+++ src/components/App.js
@@ -12,20 +12,17 @@ import ChallengesPage from '../pages/ChallengesPage';
 import MypagePage from '../pages/MypagePage';
 import ChangePasswordPage from '../pages/ChangePasswordPage';
 import PrivateRoute from '../routes/PrivateRoute';
-// admin page test
-// import Header from '../components/Header';
-// import ChallSettingPage from '../pages/ChallSettingPage';
-// import UserSettingPage from '../pages/UserSettingPage';
-// import NotiSettingPage from '../pages/NotiSettingPage';
-// import UserForm from '../components/UserForm';
-// import ChallengeForm from '../components/ChallengeForm';
+import ChallSettingPage from '../pages/ChallSettingPage';
+import UserSettingPage from '../pages/UserSettingPage';
+import NotiSettingPage from '../pages/NotiSettingPage';
+import UserForm from '../components/UserForm';
+import ChallengeForm from '../components/ChallengeForm';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
           <NavigationBar />
-          {/* <Header/> for admin page test*/}
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/login" element={<LoginPage />} />
@@ -78,8 +75,7 @@ function App() {
                 </PrivateRoute>
               }
             />
-            {/* admin page test */}
-            {/* <Route
+            <Route
               path="/challSettingPage"
               element={
                 <PrivateRoute>
@@ -106,7 +102,7 @@ function App() {
             <Route path="/edit-user/:userId" element={<UserForm />} />
             <Route path="/add-user" element={<UserForm/>} />
             <Route path="/edit-Challenge/:ChallengeId" element={<ChallengeForm />} />
-            <Route path="/add-challenge" element={<ChallengeForm/>} /> */}
+            <Route path="/add-challenge" element={<ChallengeForm/>} />
 
           </Routes>
         </Router>
