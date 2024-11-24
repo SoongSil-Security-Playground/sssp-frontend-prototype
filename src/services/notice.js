87cb9b3c685a574bb44f251ad1f2fb5571993f8a
doneni <xxerrenn@icloud.com>
2024-11-24 16:08:23 +0900

    feat: create notification service attached

--- src/services/notice.js
+++ src/services/notice.js
@@ -16,3 +16,31 @@ export const fetchNotices = async (token) => {
 
     return await response.json();
 };
+
+export const createNotice = async (title, content, token) => {
+    try {
+        const response = await fetch(`${BACKEND_URL}/api/v1/admin/notice`, {
+            method: 'POST',
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: new URLSearchParams({
+                title: title,
+                content: content,
+            }),
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.detail || 'Failed to create notice.');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error creating notice:', error.message);
+        throw error;
+    }
+};
